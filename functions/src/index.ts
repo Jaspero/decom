4e82bac6c80637063c06cc4669da8330ecdb17ab
flauc <filip.lauc93@gmail.com>
2023-12-11 00:01:13 +0100

    feat: updated rules to allow for reading users; added usercreated trigger;

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,3 +3,5 @@ import { initializeApp } from 'firebase-admin/app';
 initializeApp();
 
 export { actioncontroller } from './rest/action-controller';
+
+export { usercreated } from './triggers/user-created';
---
+++ functions/src/triggers/user-created.ts
@@ -0,0 +1,30 @@
+import {firestore} from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import {REGION} from '../shared/consts/region.const';
+
+export const usercreated = functions
+	.region(REGION)
+	.auth.user()
+	.onCreate(async user => {
+		
+		if (!user.email || user.customClaims?.role) {
+			return;
+		}
+
+		const fs = firestore();
+
+		const userDoc = await fs
+			.collection('customers')
+			.doc(user.uid)
+			.get();
+
+		if (!userDoc.exists) {
+			await fs
+				.collection('customers')
+				.doc(user.uid)
+				.set({
+					createdOn: user.metadata.creationTime,
+					email: user.email
+				});
+		}
+	});
