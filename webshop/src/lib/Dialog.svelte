7efd0e0e6889ec006dfc880be1e9d6ad68150ca7
floyyd6 <slobodantomic82@gmail.com>
2023-12-21 15:10:11 +0100

    feat: adding delete account/user #17

--- webshop/src/lib/Dialog.svelte
+++ webshop/src/lib/Dialog.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-  import { clickOutside } from '$lib/utils/click-outside';
+  import Close from 'svelte-material-icons/Close.svelte';
+  import { clickOutside } from '$lib/utils/click-outside.ts';
   import { fade } from 'svelte/transition';
   import { createEventDispatcher } from 'svelte';
 
   export let showing = false;
   export let showClose = true;
+  export let src = '';
+  export let alt = '';
   export let fullScreen = false;
   export let contentfull = false;
   export let removeMinW = false;
@@ -41,17 +44,123 @@
     <div class="blurred-bg" transition:fade={{ duration: 200 }} />
   {/if}
   <div
-    class="dialog"
-    class:fullscreen={fullScreen}
-    class:min-w-0={removeMinW}
-    class:button-outside={buttonOutside}
-    transition:fade={{ duration: 200 }}
-    use:clickOutside
-    on:click_outside={close}
+          class="dialog"
+          class:fullscreen={fullScreen}
+          class:min-w-0={removeMinW}
+          class:button-outside={buttonOutside}
+          transition:fade={{ duration: 200 }}
+          use:clickOutside
+          on:click_outside={close}
   >
+    {#if src}
+      <img {src} {alt} />
+    {/if}
     <div class="dialog-content" class:contentfull>
       <slot />
     </div>
-    <button type="button" class="dialog-close" on:click={close}> Close </button>
+    <button type="button" class="dialog-close" on:click={close}>
+      <Close size="24" />
+    </button>
   </div>
 {/if}
+
+<style>
+  .blurred-bg {
+    z-index: 2;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.8);
+    backdrop-filter: blur(3px);
+  }
+
+  .dialog {
+    z-index: 10004;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 600px;
+    max-height: 90vh;
+    transform: translate(-50%, -50%);
+    display: flex;
+    background-color: white;
+    border: 1px solid var(--primary-light);
+    border-radius: 1rem;
+    overflow-y: auto;
+  }
+  .dialog-content {
+    padding: 0;
+  }
+
+  .dialog-close {
+    position: absolute;
+    top: -1rem;
+    right: -1rem;
+    background-color: white;
+    border: 1px solid red;
+  }
+
+
+    .fullscreen {
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 0;
+      transform: translate(0);
+    }
+
+    .fullscreen.dialog-content {
+      margin: 0 auto;
+    }
+
+    .dialog-content {
+      width: 100%;
+      padding: 3rem;
+      overflow-y: auto;
+    }
+
+    .dialog-close {
+      position: absolute;
+      top: .5rem;
+      right: .5rem;
+      width: 2.5rem;
+      height: 2.5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: none;
+      background-color: transparent;
+      border-radius: 50%;
+      cursor: pointer;
+    }
+    .dialog-close:hover {
+      background-color: red;
+    }
+
+  .contentfull {
+    width: 100%;
+  }
+
+  .min-w-0 {
+    min-width: unset;
+  }
+  @media (max-width: 600px) {
+    .dialog-content {
+      width: 100%;
+      padding: 1.5rem;
+    }
+    .dialog {
+      width: 80%;
+      height: auto;
+      max-height: 90%;
+      min-width: unset;
+    }
+    img {
+      display: none;
+    }
+  }
+
+</style>
--- webshop/src/routes/my-account/settings/+page.svelte
+++ webshop/src/routes/my-account/settings/+page.svelte
@@ -1,15 +1,16 @@
 
 <script lang="ts">
-    import {updatePassword,sendEmailVerification,updateEmail} from 'firebase/auth';
+    import {updatePassword,sendEmailVerification,updateEmail,deleteUser} from 'firebase/auth';
     import {notificationWrapper} from "$lib/notification/notification.ts";
     import {auth} from "$lib/utils/firebase";
-    import {notification} from "$lib/notification/notification";
-    import {firebaseErrors} from "../../../lib/notification/notification";
+    import {firebaseErrors} from "$lib/notification/notification";
+    import Dialog from "$lib/Dialog.svelte";
 
 
 
     let newPassword = '';
     let email = '';
+    let isConfirmationVisible = false;
 
 
 const handleUpdatePassword = async () => {
@@ -46,6 +47,40 @@ const handleUpdateEmail = async () => {
         }
     }
 };
+
+    const handleDeleteAccount = () => {
+        console.log('Button clicked!');
+        isConfirmationVisible = true;
+    };
+
+    const confirmDelete = async () => {
+        isConfirmationVisible = false;
+        try {
+            if (auth.currentUser) {
+                // Delete user
+                await notificationWrapper(
+                    deleteUser(auth.currentUser),
+                    'Account deleted successfully.'
+                );
+            }
+        } catch (deleteError) {
+            console.error(deleteError);
+
+            // Handle errors using notificationWrapper
+            await notificationWrapper(
+                Promise.reject(deleteError), // Reject the promise to trigger notificationWrapper
+                firebaseErrors[deleteError.code] || 'An error occurred while deleting the account.'
+            );
+        }
+
+        console.log('Account deleted');
+    };
+
+    const cancelDelete = () => {
+        isConfirmationVisible = false;
+    };
+
+
 </script>
 
 <svelte:head>
@@ -69,4 +104,21 @@ const handleUpdateEmail = async () => {
 
         <button type="submit">Update Email</button>
     </form>
+
+    <button class="mt-[40px]" on:click={handleDeleteAccount}>Delete Account</button>
 </main>
+<Dialog bind:showing={isConfirmationVisible}>
+    <div class="w-full text-center">
+        <h1>Delete account</h1>
+        <p>This action will delete your account permanently, are you sure you want to continue?</p>
+    </div>
+    <div class="flex w-full justify-center">
+        <button class="bg-red-700 p-4 text-white rounded-lg mr-[10px]" on:click={confirmDelete}>Yes, delete my account</button>
+        <button class="bg-black text-white rounded-lg p-4" on:click={cancelDelete}>Cancel</button>
+    </div>
+</Dialog>
+
+
+<style>
+
+</style>
