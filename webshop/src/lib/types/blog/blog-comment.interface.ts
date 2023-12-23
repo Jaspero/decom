e72f3ae05738d20079645b3bcc031d51f1df5caf
flauc <filip.lauc93@gmail.com>
2023-12-24 00:47:37 +0100

    feat(webapp): connecting comments #54

---
+++ webshop/src/lib/types/blog/blog-comment.interface.ts
@@ -0,0 +1,6 @@
+export interface BlogComment {
+	author: string;
+	authorName: string;
+	content: string;
+	createdOn: string;
+}
--- webshop/src/routes/blog/[category]/[id]/+page.svelte
+++ webshop/src/routes/blog/[category]/[id]/+page.svelte
@@ -1,14 +1,109 @@
 <script lang="ts">
+  import Button from '$lib/Button.svelte';
   import BlogAuthor from '$lib/blog/BlogAuthor.svelte';
-  import { cleanSlug } from '@jaspero/utils';
-  import type { BlogArticle } from '$lib/types/blog/blog-article.interface';
-  import { meta } from '$lib/meta/meta.store';
+  import {meta} from '$lib/meta/meta.store';
+  import type {BlogArticle} from '$lib/types/blog/blog-article.interface';
+  import type {BlogComment} from '$lib/types/blog/blog-comment.interface';
+  import {authenticated, db} from '$lib/utils/firebase';
+  import {fromIso} from '$lib/utils/format-date';
+  import {cleanSlug} from '@jaspero/utils';
+  import {
+    QueryDocumentSnapshot,
+    collection,
+    getDocs,
+    limit,
+    orderBy,
+    startAt,
+
+    type DocumentData,
+
+    addDoc,
+
+    updateDoc,
+
+    doc
+
+
+
+
+  } from 'firebase/firestore';
+  import {onMount} from 'svelte';
+  import {alertWrapper} from '$lib/utils/alert-wrapper';
+  import type {User} from '$lib/types/user.interface';
 
   export let data: BlogArticle;
 
   const { author, content, image, imageAlt } = data;
+  const itemsPerPage = 10;
+
+  let comments: BlogComment[];
+  let hasMore: boolean;
+  let ref: QueryDocumentSnapshot<any>;
+  let comment = '';
+  let loading = false;
+  let commentsLoading = false;
+  let selectedComment: string | null;
 
   meta.set(data.meta);
+
+  function editComment() {}
+
+  function deleteComment() {}
+
+  async function saveComment() {
+    loading = true;
+
+    await alertWrapper(
+      selectedComment ? 
+      updateDoc(
+        doc(db, 'blog-articles', data.id, 'blog-comments', selectedComment),
+        {comment}
+      ) :
+      addDoc(
+        collection(db, 'blog-articles', data.id, 'blog-comments'),
+        {
+          author: ($authenticated as User).id,
+          comment,
+          createdOn: new Date().toISOString()
+        }
+      ),
+      'Comment saved',
+      '',
+      () => loading = false
+    )
+
+    selectedComment = null;
+    comment = '';
+    loading = false;
+  }
+
+  async function loadComments() {
+    const queries: any[] = [
+      collection(db, 'blog-articles', data.id, 'blog-comments'),
+      orderBy('createdOn', 'desc')
+    ];
+
+    if (ref) {
+      queries.push(startAt(ref));
+    }
+
+    queries.push(limit(itemsPerPage + 1));
+
+    const { docs } = await getDocs(
+      // @ts-ignore
+      ...queries
+    );
+
+    hasMore = docs.length === itemsPerPage + 1;
+    ref = docs[docs.length - 1];
+    comments.push(
+      ...docs.slice(0, itemsPerPage).map((doc) => ({ id: doc.id, ...(doc.data() as any) }))
+    );
+  }
+
+  onMount(async () => {
+    await loadComments();
+  });
 </script>
 
 <div class="grid">
@@ -56,3 +151,38 @@
     </div>
   </div>
 </div>
+
+<h4>Comments</h4>
+
+{#if comments}
+  {#if comments.length}
+    {#each comments as comment}
+      <div>
+        <span>{comment.authorName}</span>
+        <span>{fromIso(comment.createdOn)}</span>
+        <div>{comment.content}</div>
+        {#if $authenticated && comment.author === $authenticated.id}
+          <Button on:click={editComment}>Edit</Button>
+          <Button on:click={deleteComment}>Delete</Button>
+        {/if}
+      </div>
+      {#if hasMore}
+        <Button on:click={loadComments} loading={commentsLoading}>Load more</Button>
+      {/if}
+    {/each}
+  {:else}
+    <p>No comments yet</p>
+  {/if}
+{:else}
+  <p>comments loading</p>
+{/if}
+
+{#if $authenticated}
+  <form on:submit|preventDefault={saveComment}>
+    <textarea bind:value={comment} rows="5" required placeholder="Add a comment" />
+    <Button type="submit" {loading}>Save</Button>
+  </form>
+{:else}
+  <p>Sign in to comment</p>
+  <Button href="/sign-in">Sign In</Button>
+{/if}
