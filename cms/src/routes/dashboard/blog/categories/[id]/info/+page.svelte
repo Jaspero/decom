97068dca607db2c7ccb10896cb82c8f65230a324
flauc <filip.lauc93@gmail.com>
2023-12-23 20:21:24 +0100

    feat(cms): connected blog category info page closes #51

--- cms/src/routes/dashboard/blog/categories/[id]/info/+page.svelte
+++ cms/src/routes/dashboard/blog/categories/[id]/info/+page.svelte
@@ -1,27 +1,72 @@
 <script lang="ts">
-  import Card from '$lib/Card.svelte';
+  import { collection, getCountFromServer, query, where } from 'firebase/firestore';
+  import { db } from '$lib/utils/firebase';
+  import { DateTime } from 'luxon';
+  import { base64UrlEncode } from '@jaspero/utils';
   import Grid from '$lib/Grid.svelte';
   import GridCol from '$lib/GridCol.svelte';
+  import Card from '$lib/Card.svelte';
 
-  export let data: {
-    value: any;
-  };
+  export let data: any;
+
+  async function allArticles() {
+    const doc = await getCountFromServer(
+      query(collection(db, 'blog-articles'), where('category', '==', data.id))
+    );
+
+    return doc.data().count;
+  }
+
+  async function recentArticles() {
+    const doc = await getCountFromServer(
+      query(
+        collection(db, 'blog-articles'),
+        where('category', '==', data.id),
+        where('publicationDate', '>=', DateTime.now().minus({ days: 30 }).toISODate())
+      )
+    );
+
+    return doc.data().count;
+  }
 </script>
 
 <Grid>
   <GridCol span="12">
     Category Name:
     <Card>
-      <slot slot="title">{data.value.name}</slot>
+      <slot slot="title">{data.name}</slot>
     </Card>
   </GridCol>
   <div class="counter">
-    <span class="counter-number"> 72 </span>
+    <span class="counter-number">
+      {#await allArticles()}
+        -
+      {:then count}
+        {count}
+      {/await}
+    </span>
     <span class="counter-text">All articles</span>
+    <a
+      href="/dashboard/blog/articles?filters={base64UrlEncode({
+        category: data.id
+      })}">View Articles</a
+    >
   </div>
   <div class="counter">
-    <span class="counter-number"> 24 </span>
+    <span class="counter-number">
+      {#await recentArticles()}
+        -
+      {:then count}
+        {count}
+      {/await}
+    </span>
     <span class="counter-text">Recent</span>
+    <a
+      href="/dashboard/blog/articles?filters={base64UrlEncode({
+        category: data.id,
+        publicationDateStart: DateTime.now().minus({ days: 30 }).toISODate()
+      })}">View Articles</a
+    >
   </div>
 </Grid>
 
