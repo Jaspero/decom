53cebedbdc5504b6bb4f1774b1517a30a079ca02
floyyd6 <slobodantomic82@gmail.com>
2023-12-27 16:04:54 +0100

    feat: adding basic layout for coupons #16

--- webshop/src/routes/my-account/coupons/+page.svelte
+++ webshop/src/routes/my-account/coupons/+page.svelte
@@ -8,13 +8,9 @@
 
   onMount(async () => {
     try {
-      // Fetch data from Firestore collection
       const querySnapshot = await getDocs(collection(db, 'coupons'));
-
-      // Map the documents to an array of data
       items = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 
-      // Log the fetched collection data to the console
       console.log('Fetched Collection:', items);
     } catch (error) {
       console.error('Error fetching collection:', error);
@@ -22,19 +18,37 @@
   });
 </script>
 
-
-
-
-<p>Coupons</p>
-
-
 <main>
-  <h1>Your Collection</h1>
-    <ul>
-      {#each items as item (item.id)}
-        <li>{item.name}, {item.code}</li>
-      {/each}
-    </ul>
+  <h1>Your Coupons</h1>
+
+
+  <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+    {#each items as item (item.id)}
+    <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+      {item.name}
+    </h5>
+    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+      your coupon code: {item.code}
+    </p>
+    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+      your coupon description: {item.description}
+    </p>
+    {#if item.used === true}
+    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+      Your coupon is: Used
+    </p>
+      {:else } <p>pero</p>
+    {/if}
+    {#if item.active === true}
+    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+      Status: Your coupon is active
+    </p>
+    {/if}
+    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+      your coupon code: {items.expiringDAte}
+    </p>
+    {/each}
+  </div>
   {#if !items}
     <p>No items found.</p>
   {/if}
