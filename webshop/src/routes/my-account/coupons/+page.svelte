d910832de887f6f830897283edd1202525d747f7
floyyd6 <slobodantomic82@gmail.com>
2024-01-03 14:26:54 +0100

    feat:added selects for filtering #16

--- webshop/src/routes/my-account/coupons/+page.svelte
+++ webshop/src/routes/my-account/coupons/+page.svelte
@@ -1,75 +1,76 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { collection, getDocs } from 'firebase/firestore';
+  import {onMount} from 'svelte';
+  import {collection, getDocs, query, where} from 'firebase/firestore';
   import { db } from '$lib/utils/firebase.ts';
-  import { writable } from 'svelte/store';
+
 
   let items = [];
+  let usedFilter = null;
+  let activeFilter = null;
 
-  const filter = writable({
-    showAll: true,
-    statusFilter: 'showAll', // Added status filter
-    validityFilter: 'showAll', // Added validity filter
-  });
+
 
   onMount(async () => {
     try {
-      const querySnapshot = await getDocs(collection(db, 'coupons'));
-      items = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      const filterConditions = [];
 
+      if (usedFilter !== null) {
+        filterConditions.push(where('used', '==', usedFilter));
+      }
+
+      if (activeFilter !== null) {
+        filterConditions.push(where('active', '==', activeFilter));
+      }
+
+      const querySnapshot = await getDocs(query(collection(db, 'coupons'), ...filterConditions));
+
+      items = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
       console.log('Fetched Collection:', items);
     } catch (error) {
       console.error('Error fetching collection:', error);
     }
   });
 
-  function updateFilter(filterType, value) {
-    filter.update((currentFilter) => {
-      return { ...currentFilter, [filterType]: value, showAll: false };
-    });
-  }
+  $: filteredItems = items.filter((item) => {
+    const usedPass = usedFilter === null || item.used === usedFilter;
+    const activePass = activeFilter === null || item.active === activeFilter;
 
-  $: filteredItems = items.filter(item => {
-    const statusFilter = $filter.statusFilter === 'showAll' || ($filter.statusFilter === 'used' ? item.used : !item.used);
-    const validityFilter = $filter.validityFilter === 'showAll' || ($filter.validityFilter === 'active' ? isCouponActive(item.endDate) : !isCouponActive(item.endDate));
-
-    return statusFilter && validityFilter;
+    return usedPass && activePass;
   });
 
-  function copyToClipboard(text) {
+  const copyToClipboard = (text) => {
+    // Create a temporary textarea element
     const textarea = document.createElement('textarea');
     textarea.value = text;
+
+    // Append the textarea to the document
     document.body.appendChild(textarea);
+
+    // Select and copy the text
     textarea.select();
     document.execCommand('copy');
-    document.body.removeChild(textarea);
-  }
 
-  function isCouponActive(endDate) {
-    const endDateTime = new Date(endDate);
-    const currentDate = new Date();
-    return endDateTime > currentDate;
-  }
+    // Remove the textarea from the document
+    document.body.removeChild(textarea);
+  };
 
 </script>
 
 
 <main>
   <div class="w-full flex items-center">
-    <label for="statusFilter">
-      Coupon Status:
-      <select id="statusFilter" bind:value={$filter.statusFilter} on:change={() => updateFilter('statusFilter', event.target.value)}>
-        <option value="showAll">Show All</option>
-        <option value="used">Used</option>
-        <option value="unused">Unused</option>
+    <label for="usedFilter">
+      <select id="usedFilter" bind:value={usedFilter}>
+        <option value={null}>Show All</option>
+        <option value={true}>Used</option>
+        <option value={false}>Unused</option>
       </select>
     </label>
-    <label for="validityFilter">
-      Coupon Validity:
-      <select id="validityFilter" bind:value={$filter.validityFilter} on:change={() => updateFilter('validityFilter', event.target.value)}>
-        <option value="showAll">Show All</option>
-        <option value="active">Active</option>
-        <option value="inactive">Inactive</option>
+    <label for="activeFilter">
+      <select id="activeFilter" bind:value={activeFilter}>
+        <option value={null}>Show All</option>
+        <option value={true}>Active</option>
+        <option value={false}>Inactive</option>
       </select>
     </label>
   </div>
@@ -79,9 +80,7 @@
         class="{item.used
           ? 'used-card'
           : 'normal-card'} block rounded-lg bg-white coupon-card p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 mr-[10px]">
-        <h5
-          class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 text-center"
-        >
+        <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 text-center">
           {item.name}
         </h5>
         <div class="flex items-center mb-4 w-full justify-between">
