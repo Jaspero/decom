20051aae4c22cf64d491cb8ccb92a20934d0db9a
floyyd6 <slobodantomic82@gmail.com>
2024-01-04 12:47:03 +0100

    feat:added tags for coupons, added tailwind media queries in tailwind.config.js #16

--- webshop/src/routes/my-account/coupons/+page.svelte
+++ webshop/src/routes/my-account/coupons/+page.svelte
@@ -7,7 +7,7 @@
   let items = [];
   let usedFilter = null;
   let activeFilter = null;
-
+  let currentDate = new Date();
 
 
   onMount(async () => {
@@ -54,10 +54,12 @@
     document.body.removeChild(textarea);
   };
 
+
+
 </script>
 
 
-<main>
+<div class="flex-1">
   <div class="w-full flex items-center">
     <label for="usedFilter">
       <select id="usedFilter" bind:value={usedFilter}>
@@ -74,69 +76,77 @@
       </select>
     </label>
   </div>
-  <div class="flex flex-wrap justify-start align-start gap-4">
+  <div class="flex flex-wrap justify-start items-stretch  gap-4">
     {#each filteredItems as item (item.id)}
       <div
-        class="{item.used
-          ? 'used-card'
-          : 'normal-card'} block rounded-lg bg-white coupon-card p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 mr-[10px]">
-        <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 text-center">
-          {item.name}
-        </h5>
-        <div class="flex items-center mb-4 w-full justify-between">
+        class="{item.used ? 'used-card' : 'coupon-card'} block rounded-lg bg-white w-[45%] xl:w-full shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        <div class="flex flex-wrap h-full">
+        <div class="flex flex-col flex-wrap justify-start flex-1 gap-4 p-6">
+          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 text-center w-full">
+            {item.name}
+          </h5>
+          <div class="w-full flex justify-between">
+            <p class="text-base text-neutral-600 dark:text-neutral-200">
+              <b>Coupon code:</b>
+              {item.code}
+            </p>
+            <button on:click={() => copyToClipboard(item.code)} disabled={item.used}>
+              <img src="/images/copy.svg" alt="" class="w-[25px]" />
+            </button>
+          </div>
           <p class="text-base text-neutral-600 dark:text-neutral-200">
-            <b>Coupon code:</b>
-            {item.code}
+            <b>Coupon type</b>: {item.type}
           </p>
-          <button on:click={() => copyToClipboard(item.code)} disabled={item.used}>
-            <img src="/images/copy.svg" alt="" class="w-[25px]" />
-          </button>
+          {#if item.description}
+            <p class="text-base text-neutral-600 dark:text-neutral-200">
+              {item.description}
+            </p>
+          {/if}
+          <p class="text-base text-neutral-600 dark:text-neutral-200">
+            <b>Coupon ends:</b>
+            {item.endDate} at {item.endTime}
+          </p>
+          <div class="flex flex-wrap items-end flex-1 gap-2">
+            {#if item.used === true}
+              <p class="text-base text-neutral-600 dark:text-neutral-200">
+                <b>This coupon is: </b>Used
+              </p>
+            {/if}
+            {#if item.active === true}
+              <p class="text-base text-neutral-600 dark:text-neutral-200">
+                <span class="tag-active bg-black text-white p-2 rounded-[16px]">Active</span>
+              </p>
+            {/if}
+            {#if item.active === false && item.endDate === ""}
+              <p class="text-base text-neutral-600 dark:text-neutral-200">
+                <span class="bg-[#980C0C7F] text-white p-2 rounded-[16px]">Inactive</span>
+              </p>
+            {/if}
+            {#if item.used === false && item.active === false && item.setEndDate === true }
+              <p class="text-base text-neutral-600 dark:text-neutral-200">
+                <span class="bg-[#757575FF] text-white p-2 rounded-[16px]">Unused</span>
+              </p>
+            {/if}
+          </div>
         </div>
-        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          <b>Coupon discount:</b>
-          {item.percent}%
-        </p>
-        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          <b>coupon type</b>: {item.type}
-        </p>
-        {#if item.description}
-          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-            {item.description}
-          </p>
-        {/if}
-        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          <b>Coupon ends:</b>
-          {item.endDate} at {item.endTime}
-        </p>
-        {#if item.used === true}
-          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-            <b>This coupon is: </b>Used
-          </p>
-        {/if}
-        {#if item.active === true}
-          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-            Status: Your coupon is active
-          </p>
-        {/if}
+          <div class="flex justify-center flex-col items-center gap-2 w-[30%] border-solid border-l-[1px] border-black">
+            <div class="bg-[#4caf50] text-white p-4 rounded-full">
+              {item.percent}% off
+            </div>
+          </div>
+        </div>
       </div>
     {/each}
   </div>
   {#if !items}
     <p>No items found.</p>
   {/if}
-</main>
-
+</div>
 <svelte:head>
   <title>Coupons - Jaspero</title>
 </svelte:head>
 
 <style lang="postcss">
-
-.coupon-card {
-    max-width: 300px;
-    width: 100%;
-  }
-
   .used-card {
     opacity: 0.7;
     text-decoration: line-through;
