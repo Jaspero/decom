a19f6273bd9da4d8be85becbd2646c7abcb97dd2
koleCar <h.kolaric92@gmail.com>
2024-01-26 15:32:56 +0100

    fix: order display

--- webshop/src/routes/my-account/orders/+page.svelte
+++ webshop/src/routes/my-account/orders/+page.svelte
@@ -1,4 +1,25 @@
-<p>Orders</p>
+<script lang="ts">
+  import { db, user } from "$lib/utils/firebase";
+  import { collection, getDoc, limit, orderBy, query, where } from 'firebase/firestore';
+  import { onMount } from "svelte";
+  import Product from "$lib/Product.svelte";
+
+  const sizeLimit = 5
+  let favoriteProducts = [];
+
+  onMount(async () => {
+    if ($user) {
+      query(
+              collection(db, 'packages'),
+              orderBy('orders'),
+              where('customerId', '==', $user.id),
+              limit(sizeLimit)
+      );
+
+    }
+  });
+</script>
+
 
 <svelte:head>
   <title>Orders - Jaspero</title>
